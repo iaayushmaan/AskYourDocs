1869ade86a605efa0f5c93823acb223040f8c477
Aayushmaan Tawer <116517217+iaayushmaan@users.noreply.github.com>
2025-01-02 02:15:05 +0530

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './components/header';
+import Chatbot from './components/chatbot';
+
+
+function App() {
+    return (
+        <div>
+            <Header/>
+            <Chatbot/>
+        </div>
+    );
+}
+
+export default App;
